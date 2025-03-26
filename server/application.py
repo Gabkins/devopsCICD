220f4923173353aa1f553b5c8e3ddc1a82b7a879
gabkanis <shishasws@gmail.com>
2025-03-26 13:14:44 +0000

    :c

--- server/application.py
+++ server/application.py
@@ -3,14 +3,17 @@ import socketserver
 
 PORT = 8000
 
-class TestMe():
-	def take_five(self):
-		return 5
-	def port(self):
-		return PORT
+class TestMe:
+    def take_five(self):
+        return 5
+
+    def port(self):
+        return PORT
+
 if __name__ == '__main__':
-	Handler = http.server.SimpleHTTPRequestHandler
-	with socketserver.TCPServer(("", PORT), Handler) as http:
-		print("serving at port", PORT)
-		http.serve_forever()
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as http:
+        print("serving at port", PORT)
+        http.serve_forever()
 
